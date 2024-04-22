9152b7f9961d0370f61f20b2645bc9958c08cefb
Khero <kherozhen@gmail.com>
2024-04-22 15:35:40 +0200

    Add Logement.jsx & .scss

--- application_web_location_immo_react/src/Pages/Home/Home.jsx
+++ application_web_location_immo_react/src/Pages/Home/Home.jsx
@@ -1,6 +1,9 @@
-
+import React from "react";
+import { Link } from 'react-router-dom';
+import jsonData from '../../App.json';
 
 function Home() {
+
   return (
     <div className="home">
       <div className="banner">
@@ -8,9 +11,16 @@ function Home() {
       </div>
       
       <div className="locations">
-        <article className="locationCard">
-          <h2>Titre de la location</h2>
-        </article>
+        {jsonData.map((item, index) => (
+          <Link to="/locationCard">
+            <article className="locationCard" key={index}>
+              <img src={item.cover} alt={item.title} />
+              <div>
+                <h2>{item.title}</h2>
+              </div>
+            </article>
+          </Link>
+          ))}
        
       </div>
     
--- application_web_location_immo_react/src/Pages/Home/Logement.jsx
+++ application_web_location_immo_react/src/Pages/Home/Logement.jsx
@@ -2,8 +2,37 @@
 
 function Logement() {
     return (
-        <div>
+        <div className="logements">
+            <img className="bannerLogement"></img>
+            <div className="titleLocation">
+                <div className="location">
+                    <h1></h1>
+                    <h2></h2>
+                </div>
+                <div className="owner">
+                    <div className="ownerText">
+                        <p className="name"></p>
+                        <p className="lastName"></p>
+                    </div>
+                    <img  />
+                </div>
+            </div>
 
+            <div className="spe-rating">
+                <div className="specification">
+                    <p></p>
+                    </div>
+                <div className="rating"></div>
+            </div>
+
+            <div className="collapseLogement">
+                <div className="description">
+                    <p>Description</p>  
+                </div>
+                <div className="description">
+                    <p>Équipements</p>  
+                </div>
+            </div>
         </div>
     )
 }
