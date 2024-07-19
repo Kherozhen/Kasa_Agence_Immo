60fcd74dbec393c50e7f6da5163f8b043c18ef7e
Khero <kherozhen@gmail.com>
2024-07-19 22:01:10 +0200

    correction

--- application_web_location_immo_react/src/Pages/Error/ErrorPage.jsx
+++ application_web_location_immo_react/src/Pages/Error/ErrorPage.jsx
@@ -8,7 +8,7 @@ function ErrorPage () {
             <h1>404</h1>
             <p>Oups! La page que vous demandez n'existe pas.</p>
 
-            <Link to="/">Retourner sur la page d'acceuil</Link>
+            <Link to="/">Retourner sur la page d'accueil</Link>
 
         </div>
     )
